786348ef710c687df7d2b8cb43618e4dc0a87a17
Yurii Sulima <yurijsulima876@gmail.com>
2025-04-16 13:49:17 +0300

    Додав видалення сповіщень

---
+++ src/main/java/com/example/shop/controller/OfferController.java
@@ -0,0 +1,17 @@
+package com.example.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+//@Controller
+//public class OfferController {
+//
+//    @PostMapping("/exchange/make-offer/${id}")
+//    public String makeOffer(@PathVariable String id){
+//        Long iD = Long.parseLong(id.replace("\u00A0", ""));
+//
+//        return "redirect:/products/${iD}";
+//    }
+//
+//}
